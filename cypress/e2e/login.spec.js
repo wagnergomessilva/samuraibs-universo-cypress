db9851f297c338ca3f82b2457ae72ff1531b964c
Wagner <wag.gomes2015@gmail.com>
2024-01-04 19:46:02 -0300

    Criado caso de teste para testar a funcionalidade de login

---
+++ cypress/e2e/login.spec.js
@@ -0,0 +1,22 @@
+
+import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
+
+describe('login', function () {
+    context('quando o usuário é muito bom', function () {
+
+        const user = {
+            name: 'Robson Jassa',
+            email: 'jassa@samuraibs.com',
+            password: 'pwd123'
+        }
+
+        it('deve logar com sucesso', function () {
+            loginPage.go()
+            loginPage.form(user)
+            loginPage.submit()
+
+            dashPage.header.userLoggedIn(user.name)
+        })
+    })
+})
