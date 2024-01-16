e6c48022da57257816d655fae0396fe0b675e798
Wagner <wag.gomes2015@gmail.com>
2024-01-16 20:33:35 -0300

    Implementado toda a preparação do contexto do teste para validação de agendamento

--- cypress/e2e/dash.spec.js
+++ cypress/e2e/dash.spec.js
@@ -1,4 +1,4 @@
-
+import loginPage from '../support/pages/login'
 
 describe('dashboard', function () {
     context('quando o cliente faz um agendamento no APP mobile', function () {
@@ -10,7 +10,7 @@ describe('dashboard', function () {
                 password: 'pwd123',
                 is_provider: false
             },
-            samurai: {
+            provider: {
                 name: 'Ramom Valdez',
                 email: 'ramom@televisa.com',
                 password: 'pwd123',
@@ -19,19 +19,78 @@ describe('dashboard', function () {
         }
 
         beforeEach(function () {
+            cy.postUser(data.provider)
             cy.postUser(data.customer)
-            cy.postUser(data.samurai)
 
             cy.apiLogin(data.customer).then(function () {
                 cy.log('Conseguimos pegar o Token ' + Cypress.env('apiToken'))
             })
+
+            cy.setProviderId(data.provider.email)
+            cy.createAppointment()
         })
 
         it('o mesmo deve ser exibido no dashboard', function () {
-            console.log(data)
+
+            loginPage.go()
+            loginPage.form(data.provider)
+            loginPage.submit()
+
+            cy.wait(3000)
         })
 
     })
+})
+
+import moment from 'moment'
+
+Cypress.Commands.add('createAppointment', function () {
+
+    let now = new Date()
+    now.setDate(now.getDate() + 1)
+
+    const date = moment(now).format('YYYY-MM-DD 14:00:00')
+
+    const payload = {
+        provider_id: Cypress.env('providerId'),
+        date: date
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/appointments',
+        body: payload,
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+    })
+
+})
+
+
+Cypress.Commands.add('setProviderId', function (providerEmail) {
+
+    cy.request({
+        method: 'GET',
+        url: 'http://localhost:3333/providers',
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+        console.log(response.body)
+
+        const providerlist = response.body
+
+        providerlist.forEach(function (provider) {
+            if (provider.email === providerEmail) {
+                Cypress.env('providerId', provider.id)
+            }
+        });
+    })
+
 })
 
 Cypress.Commands.add('apiLogin', function (user) {
