1917a8f93954c5333a15cb2e3ee473016cb31290
Wagner <wag.gomes2015@gmail.com>
2024-01-23 21:05:01 -0300

    Finalizado automação do cenário que valida o agendamento no front do dashboard

--- cypress/e2e/dash.spec.js
+++ cypress/e2e/dash.spec.js
@@ -1,4 +1,5 @@
 import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
 
 describe('dashboard', function () {
     context('quando o cliente faz um agendamento no APP mobile', function () {
@@ -15,19 +16,17 @@ describe('dashboard', function () {
                 email: 'ramom@televisa.com',
                 password: 'pwd123',
                 is_provider: true
-            }
+            },
+            appointmentHour: '14:00'
         }
 
-        beforeEach(function () {
+        before(function () {
             cy.postUser(data.provider)
             cy.postUser(data.customer)
 
-            cy.apiLogin(data.customer).then(function () {
-                cy.log('Conseguimos pegar o Token ' + Cypress.env('apiToken'))
-            })
-
+            cy.apiLogin(data.customer)
             cy.setProviderId(data.provider.email)
-            cy.createAppointment()
+            cy.createAppointment(data.appointmentHour)
         })
 
         it('o mesmo deve ser exibido no dashboard', function () {
@@ -36,76 +35,12 @@ describe('dashboard', function () {
             loginPage.form(data.provider)
             loginPage.submit()
 
-            cy.wait(3000)
+            dashPage.calendarShouldBeVisible()
+
+            const day = Cypress.env('appointmentDay')
+            dashPage.selectDay(day)
+
+            dashPage.appointmentShouldBe(data.customer, data.appointmentHour)
         })
-
-    })
-})
-
-import moment from 'moment'
-
-Cypress.Commands.add('createAppointment', function () {
-
-    let now = new Date()
-    now.setDate(now.getDate() + 1)
-
-    const date = moment(now).format('YYYY-MM-DD 14:00:00')
-
-    const payload = {
-        provider_id: Cypress.env('providerId'),
-        date: date
-    }
-
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3333/appointments',
-        body: payload,
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-    })
-
-})
-
-
-Cypress.Commands.add('setProviderId', function (providerEmail) {
-
-    cy.request({
-        method: 'GET',
-        url: 'http://localhost:3333/providers',
-        headers: {
-            authorization: 'Bearer ' + Cypress.env('apiToken')
-        }
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-        console.log(response.body)
-
-        const providerlist = response.body
-
-        providerlist.forEach(function (provider) {
-            if (provider.email === providerEmail) {
-                Cypress.env('providerId', provider.id)
-            }
-        });
-    })
-
-})
-
-Cypress.Commands.add('apiLogin', function (user) {
-
-    const payload = {
-        email: user.email,
-        password: user.password
-    }
-
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3333/sessions',
-        body: payload
-    }).then(function (response) {
-        expect(response.status).to.eq(200)
-        Cypress.env('apiToken', response.body.token)
     })
 })
--- cypress/support/pages/dash/index.js
+++ cypress/support/pages/dash/index.js
@@ -7,6 +7,25 @@ class DashPage {
         this.header = header
     }
 
+    calendarShouldBeVisible() {
+        cy.get(el.calendar, { timeout: 7000 })
+            .should('be.visible')
+    }
+
+    selectDay(day) {
+        const target = new RegExp('^' + day + '$', 'g')
+        cy.contains(el.boxDay, target)
+            .click({ force: true })
+    }
+
+    appointmentShouldBe(customer, hour) {
+        cy.contains('div', customer.name)
+            .should('be.visible')
+            .parent()
+            .contains(el.boxHour, hour)
+            .should('be.visible')
+    }
+
 }
 
 export default new DashPage()
